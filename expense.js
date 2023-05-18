36c0f60145263183ec0c1e92a1928c54c7ad6265
Sergey Moryakov <sergey@moryakov.com>
2023-05-18 14:01:18 +0300

    develop Spent List section

--- expense.js
+++ expense.js
@@ -1,1 +1,32 @@
-console.log("test comment");
+// Create list of expenses
+const expenses = [];
+
+const expenseAdderNode = document.querySelector('#expenseAdder');
+const expenseAdderButtonNode = document.querySelector('#expenseAdderButton');
+const spentListNode = document.querySelector('#spentList')
+
+expenseAdderButtonNode.addEventListener('click', function() {
+    // Get input field value and check for integer
+    if (!expenseAdderNode.value) {
+        console.log('Error: Empty value');
+        return;
+    }
+
+    const amount = parseInt(expenseAdderNode.value);
+    expenseAdderNode.value = '';
+    
+    // Add value amout to list of expenses
+    expenses.push(amount);
+
+    console.log(expenses);
+    
+    // Display list of expenses
+    let spentListHTML = '';
+
+    expenses.forEach(element => {
+        spentListHTML = spentListHTML + `<li>${element}</li>`;
+    });
+    
+    console.log('SpentListHTML: ', spentListHTML);
+    spentListNode.innerHTML = `<ol>${spentListHTML}</ol>`;
+});
